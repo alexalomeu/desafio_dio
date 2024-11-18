1f17c157d784136b5c4bc8846e8b4b7b544e9cc7
alelomeu <alexsandra.lomeu@gigaservices.com.br>
2024-11-18 14:36:03 -0300

    first commit

---
+++ desafio.rb
@@ -0,0 +1,10 @@
+puts "Digite seu nome:"
+nome = gets.chomp
+puts "Seu nome é #{nome}"
+puts "Digite seu sobrenome:"
+sobrenome = gets.chomp
+puts "Seu sobrenome é #{sobrenome}"
+puts "Digite sua idade:"
+idade = gets.chomp
+puts "Sua idade é #{idade}"
+puts "Seu nome completo é #{nome} #{sobrenome} e você tem #{idade} anos "
